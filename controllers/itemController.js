1744653a308de4490e448d509a9c5f1e145fb7ff
nanu <kuelupis31@gmail.com>
2022-09-30 16:07:16 +0700

    edit app.js

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -138,6 +138,7 @@ const postItem = async (req, res) => {
       return res.status(200).json(data);
     })
     .catch((err) => {
+      console.log(err);
       return res.status(500).json(err);
     });
 };
